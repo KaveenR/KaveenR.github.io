d438f4a27fa7b7265344ed16139a5a1926c3be58
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2019-05-02 19:19:23 +0530

    updates

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $("#hack").hover(function(){
-    $("#hackit").addClass("flipped");
-    $("#name").text("Rest In Peace Tiger")
-  },function(){
-    $("#hackit").removeClass("flipped");
-    $("#name").text("Kaveen Rodrigo")
-  });
-});
